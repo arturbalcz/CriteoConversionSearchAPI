55fcebf00d60c7a9c7d41d14cf839fd9474b7ce6
Artur Balczynski <artur.balczynski@student.put.poznan.pl>
2020-12-02 01:40:32 +0100

    importing data from csv to db

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import csv
 import mysql.connector as connector
-from model.ClickEntity import ClickEntity
+from model.click_entity import drop_table_script, create_table_script, insert_entity_script
 
 
 def import_data_from_csv():
+    filename = 'resource/ccs_top_100.csv'
+
     db = connector.connect(host='localhost', port='33069', password='q1w2e3r4', user='root', database='ccs')
     cursor = db.cursor()
-    click_entity = ClickEntity.create_from_csv_record("1	89.9	442485	1598929598	9	49.95")
-    cursor.execute(ClickEntity.drop_table_script)
-    cursor.execute(ClickEntity.create_table_script)
-    cursor.execute(ClickEntity.insert_entity_script, click_entity)
+    cursor.execute(drop_table_script)
+    cursor.execute(create_table_script)
+
+    with open(filename, 'r') as file:
+        reader = csv.reader(file, delimiter='\t')
+        for row in reader:
+            cursor.execute(insert_entity_script, tuple(row))
     db.commit()
 
 
---
+++ model/click_entity.py
@@ -0,0 +1,57 @@
+drop_table_script = "DROP TABLE IF EXISTS CLICK;"
+create_table_script = "CREATE TABLE CLICK (" \
+                      "ID INT AUTO_INCREMENT PRIMARY KEY," \
+                      "SALE INT," \
+                      "SALES_AMOUNT FLOAT," \
+                      "TIME_DELAY_FOR_CONVERSION INT," \
+                      "CLICK_TIMESTAMP INT," \
+                      "NB_CLICKS_ONE_WEEK INT," \
+                      "PRODUCT_PRICE FLOAT," \
+                      "PRODUCT_AGE_GROUP VARCHAR(32)," \
+                      "DEVICE_TYPE VARCHAR(32)," \
+                      "AUDIENCE_ID VARCHAR(32)," \
+                      "PRODUCT_GENDER VARCHAR(32)," \
+                      "PRODUCT_BRAND VARCHAR(32)," \
+                      "PRODUCT_CATEGORY_1 VARCHAR(32)," \
+                      "PRODUCT_CATEGORY_2 VARCHAR(32)," \
+                      "PRODUCT_CATEGORY_3 VARCHAR(32)," \
+                      "PRODUCT_CATEGORY_4 VARCHAR(32)," \
+                      "PRODUCT_CATEGORY_5 VARCHAR(32)," \
+                      "PRODUCT_CATEGORY_6 VARCHAR(32)," \
+                      "PRODUCT_CATEGORY_7 VARCHAR(32)," \
+                      "PRODUCT_COUNTRY VARCHAR(32)," \
+                      "PRODUCT_ID VARCHAR(32)," \
+                      "PRODUCT_TITLE VARCHAR(512)," \
+                      "PARTNER_ID VARCHAR(32)," \
+                      "USER_ID VARCHAR(32)" \
+                      ");"
+insert_entity_script = "INSERT INTO CLICK (" \
+                       "SALE, " \
+                       "SALES_AMOUNT," \
+                       "TIME_DELAY_FOR_CONVERSION," \
+                       "CLICK_TIMESTAMP," \
+                       "NB_CLICKS_ONE_WEEK," \
+                       "PRODUCT_PRICE," \
+                       "PRODUCT_AGE_GROUP," \
+                       "DEVICE_TYPE," \
+                       "AUDIENCE_ID," \
+                       "PRODUCT_GENDER," \
+                       "PRODUCT_BRAND," \
+                       "PRODUCT_CATEGORY_1," \
+                       "PRODUCT_CATEGORY_2," \
+                       "PRODUCT_CATEGORY_3," \
+                       "PRODUCT_CATEGORY_4," \
+                       "PRODUCT_CATEGORY_5," \
+                       "PRODUCT_CATEGORY_6," \
+                       "PRODUCT_CATEGORY_7," \
+                       "PRODUCT_COUNTRY," \
+                       "PRODUCT_ID," \
+                       "PRODUCT_TITLE," \
+                       "PARTNER_ID," \
+                       "USER_ID)" \
+                       "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+
+def create_click_tuple_from_csv_record(line: str, separator='\t'):
+    values = line.split(separator)
+    return tuple(values)
