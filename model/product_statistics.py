b6a46729adc62fd6f148d1907a6fe4ddc458bdfe
Artur Balczynski <artur.balczynski@student.put.poznan.pl>
2021-01-11 01:21:02 +0100

    added product id sorting in results files

--- model/product_statistics.py
+++ model/product_statistics.py
@@ -5,4 +5,4 @@ class ProductStatistics:
     MEAN = 3
     STD_DEV = 4
 
-    select_products_statistics_where_partner_id_and_day_script = 'SELECT PRODUCT_ID, DAILY_PROFIT, DAILY_COST, MEAN, STD_DEV FROM PRODUCT_DAY JOIN PRODUCT_DAY_MEAN PDM ON PRODUCT_DAY.ID = PDM.PRODUCT_DAY_ID WHERE PARTNER_ID = %(partner_id)s AND DAY_DATE = %(date)s;'
+    select_products_statistics_where_partner_id_and_day_script = 'SELECT PRODUCT_ID, DAILY_PROFIT, DAILY_COST, MEAN, STD_DEV FROM PRODUCT_DAY JOIN PRODUCT_DAY_MEAN PDM ON PRODUCT_DAY.ID = PDM.PRODUCT_DAY_ID WHERE PARTNER_ID = %(partner_id)s AND DAY_DATE = %(date)s ORDER BY PRODUCT_ID;'
